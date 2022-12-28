507bfb2f7fceabe07ed26c1d05637343227211f3
kpfoundation <project.kpf@gmail.com>
2022-12-28 17:57:06 +0530

    Commiting Changes in API url

--- src/API.js
+++ src/API.js
@@ -1,5 +1,5 @@
 // for localhost
-export const API = 'http://localhost:5000'
+// export const API = 'http://localhost:5000'
 
 // Cyclic url
 //export const API = '"https://proud-pink-calf.cyclic.app"'
@@ -7,3 +7,5 @@ export const API = 'http://localhost:5000'
 //netlify
 
 //export const API = 'https://6380ce1057370922961769a7--kpf-api.netlify.app/
+
+export const API = 'https://kpfapi.checkapp.one'
