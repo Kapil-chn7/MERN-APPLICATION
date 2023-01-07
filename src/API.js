43b8b51d5976f9ba8fd11b00edbd6423ae41fe2f
kpfoundation <project.kpf@gmail.com>
2023-01-07 06:42:41 +0530

    changing url

--- src/API.js
+++ src/API.js
@@ -1,11 +1,8 @@
 // for localhost
 //export const API = 'http://localhost:5000'
 
-// Cyclic url
-export const API = '"https://proud-pink-calf.cyclic.app"'
-
 //netlify
 
 //export const API = 'https://6380ce1057370922961769a7--kpf-api.netlify.app/
 
-//export const API = 'https://kpfapi.checkapp.one'
+export const API = 'https://kpfapi.checkapp.one'
